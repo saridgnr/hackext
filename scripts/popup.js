07a8c282161c27b40c8067623c299ba0b09712b2
Noa <saridgnr@gmail.com>
2018-05-31 23:41:18 +0300

    invert

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,7 +1,9 @@
 var fontSize = 1;
+var isIverted = false;
 
 $(document).ready(function(){
     let fontBigger = document.getElementById('format-size');
+    let invertColors = document.getElementById('invert');
     fontBigger.onclick = function (element) {
         fontSize += 0.2;
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -11,18 +13,33 @@ $(document).ready(function(){
 
         });
     };
+
+    invertColors.onclick = function(element){
+        if(!isIverted) {
+            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                chrome.tabs.executeScript(
+                    tabs[0].id,
+                    { code: 'document.body.style.filter = "invert(100%)";' });
+    
+            });
+
+            isIverted = true;
+        }
+        else {
+            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                chrome.tabs.executeScript(
+                    tabs[0].id,
+                    { code: 'document.body.style.filter = "invert(0%)";' });
+    
+            });
+
+            isIverted = false;
+        }
+    };
 });
 
-infoElem.onclick = function(){
-    let color = "#F0F8FF"; 
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            {code: 'document.body.style.backgroundColor = "' + color + '";'});
-      });
   
       //alertify.alert('Ready!');
-  };
 
 
 // // let changeColor = document.getElementById('changeColor');
