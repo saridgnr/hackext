46aa8ffef74aa6f8a1fcc291c3e5c83920f10ab8
noam vaisbuch <noam.vaisbuch@gmail.com>
2018-06-01 02:13:56 +0300

    Created and designed the text desxriptions in the popUp

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,6 +1,38 @@
 var fontSize = 1;
 var isIverted = false;
 
+const descriptions = {
+    textSizeMessage: "Change the size of the text according to your selection",
+    ReadContentMessage: "Get your auditory output devices o read the highlighted text",
+    invertColorsMessage: "Invert the colors to seethe sites better",
+    profileMessage: "Define your personal profile of all our available features",
+    informationMessage: "Get all of the neccessary information about our extension",
+    mouseVibrationMessage: "Get your mouse to vibrate with the arduino component" +
+                           " whenever you mouse over a clickable object"
+                           
+  };
+
+function displayInfo(messageName) {
+    let displayedTextBox = document.getElementById("info_text");
+    displayedTextBox.innerText = descriptions[messageName];
+}
+
+(function initiateOnHovers() {
+    let formatSize = document.getElementById('format-size');
+    let volume = document.getElementById('volume');
+    let invert = document.getElementById('invert');
+    let profile = document.getElementById('profile');
+    let mouse = document.getElementById('mouse');
+    let info = document.getElementById('info');
+
+    formatSize.onmouseenter = (function() {displayInfo('textSizeMessage')});
+    volume.onmouseenter = (function() {displayInfo('ReadContentMessage')});
+    invert.onmouseenter = (function() {displayInfo('invertColorsMessage')});
+    profile.onmouseenter = (function() {displayInfo('profileMessage')});
+    mouse.onmouseenter = (function() {displayInfo('mouseVibrationMessage')});
+    info.onmouseenter = (function() {displayInfo('informationMessage')});
+})();
+
 $(document).ready(function(){
     let fontBigger = document.getElementById('format-size');
     let invertColors = document.getElementById('invert');
@@ -11,7 +43,7 @@ $(document).ready(function(){
                 tabs[0].id,
                 { code: 'document.body.style.fontSize = "' + fontSize + 'em ";' });
 
-        });setProperty ("color", "green", "important");
+        });
     };
 
     invertColors.onclick = function(element){
