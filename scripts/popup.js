42a9bcac37e3c55f41cac896d955813f85b321e9
noam vaisbuch <noam.vaisbuch@gmail.com>
2018-05-31 23:33:17 +0300

    Uneccary changes

--- scripts/popup.js
+++ scripts/popup.js
@@ -64,7 +64,7 @@ infoElem.onclick = function(){
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.executeScript(
           tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '";'});
+          {code: 'alertify.alert("Ready!");'});
     });
 
     //alertify.alert('Ready!');
